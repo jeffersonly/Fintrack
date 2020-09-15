2ac606c11ab041cd1b6c79fdff9c973c1ae001f0
Jefferson Ly <32202953+jeffersonly@users.noreply.github.com>
2020-09-14 19:32:28 -0700

    UI for landing complete, connect backend & errs

--- src/Components/Modals/ForgotResetPwdModal.js
+++ src/Components/Modals/ForgotResetPwdModal.js
@@ -1,12 +1,15 @@
 import './AuthModal.css';
 import React, {useState, useEffect} from 'react';
 import { Modal } from 'react-bootstrap';
+import InputField from '../InputFields/InputField';
 
 function ForgotResetPwdModal(props) {
+    //modal related states
     const [show, setShow] = useState(props.openModal);
     const [forgotPwdTabSelected, setForgotPwdTabSelected] = useState(props.forgotPwdModalShown);
     const [resetPwdTabSelected, setResetPwdTabSelected] = useState(props.resetPwdModalShown);
 
+    //Handle Modal Events 
     function closeModal() {
         props.onChangeModalState();
     }
@@ -29,21 +32,63 @@ function ForgotResetPwdModal(props) {
         setResetPwdTabSelected(props.resetPwdModalShown);
     }, [props.openModal, props.forgotPwdModalShown, props.resetPwdModalShown]);
 
-    //Authentication Events (Login, Register, Confirm Account)
-    function handleForgotPwd() {
-        console.log("Forgot Password Event");
+    //Handle Input Data
+    const [data, setData] = React.useState({});
+
+    //generate react refs for input fields based on tab selected
+    function generateReactRefs() {
+        if(forgotPwdTabSelected) {
+            return [React.createRef()];
+        }
+
+        return [React.createRef(), React.createRef(), React.createRef(), React.createRef()];
     }
 
-    function handleResetPwd() {
-        console.log("Reset Password Event");
+    //inputref needed for each input to reference it
+    const inputRefs = React.useRef(generateReactRefs());
+
+    //update data hook with input data
+    const handleChange = (name, value) => {
+        setData(prevData => ({...prevData, [name]: value}));
     }
 
+    //submit form event, checks to see if form has errors
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        //check to see if there are errors (input validation)
+        let isValid = true;
+
+        for(let i = 0; i < inputRefs.current.length; i++) {
+            if(inputRefs.current[i].current === null) { break; }
+            const valid = inputRefs.current[i].current.validate();
+            if(!valid) {
+                isValid = false;
+            }
+        }
+
+        if(!isValid) {
+            return;
+        }
+
+        //if no errors => do actions on form submit based on what modal is active (Login, Register, Confirm)
+        if(forgotPwdTabSelected) {
+            console.log("Forgot password Action");
+        }
+
+        if(resetPwdTabSelected) {
+            console.log("Reset Password Action");
+        }
+        console.log(data);
+    }
+
+    //renders footer buttons that connect modals and do auth actions
     function renderModalFooterBtns() {
         if(forgotPwdTabSelected) {
             return (
                 <>
-                    <button className="modal-link-btn" onClick={openLoginRegisterModal}>Back</button>
-                    <button className="modal-action-btn" onClick={handleForgotPwd}>Submit</button>
+                    <button className="modal-link-btn" type="button" onClick={openLoginRegisterModal}>Back</button>
+                    <button className="modal-action-btn" type="submit">Submit</button>
                 </>
             )
         }
@@ -51,7 +96,77 @@ function ForgotResetPwdModal(props) {
         if(resetPwdTabSelected) {
             return (
                 <>
-                    <button className="modal-action-btn" onClick={handleResetPwd}>Reset</button>
+                    <button className="modal-action-btn" type="submit">Reset</button>
+                </>
+            )
+        }
+
+        return null;
+    }
+
+    //renders input fields based on modal selected
+    function renderInputFields() {
+        if(forgotPwdTabSelected) {
+            return (
+                <>
+                    <InputField 
+                        ref={inputRefs.current[0]}
+                        name="username"
+                        label="Enter Username"
+                        onChange={handleChange}
+                        validation={"required|min:1|max:128"}
+                    />
+
+                    <p className="tab-informative-text">
+                        Enter the username of the account for which you want to recover.
+                        <br/>
+                        A confirmation code will be sent to the email correlated to the account. 
+                        <br/>
+                        Use the confirmation code to reset your passsword.
+                    </p>
+                </>
+            )
+        }
+
+        if(resetPwdTabSelected) {
+            return (
+                <>
+                    <InputField 
+                        ref={inputRefs.current[0]}
+                        name="username"
+                        label="Enter Username"
+                        onChange={handleChange}
+                        validation={"required|min:1|max:128"}
+                    />
+
+                    <InputField 
+                        ref={inputRefs.current[1]}
+                        name="confirmation code"
+                        label="Enter Confirmation Code"
+                        onChange={handleChange}
+                    />
+
+                    <InputField 
+                        ref={inputRefs.current[2]}
+                        name="new-password"
+                        label="Enter New Password"
+                        type="password"
+                        onChange={handleChange}
+                        validation={"required|min:8|max:99"}
+                    />  
+
+                    <InputField 
+                        ref={inputRefs.current[3]}
+                        name="confirm-new-password"
+                        label="Confirm New Password"
+                        type="password"
+                        onChange={handleChange}
+                        validation={"required|min:8|max:99"}
+                    />
+
+                    <p className="tab-informative-text">
+                        Upon successfully reseting your password, you will be prompted to log in!
+                    </p>
                 </>
             )
         }
@@ -78,13 +193,14 @@ function ForgotResetPwdModal(props) {
                     </button>
                 </Modal.Header>
 
-                <Modal.Body>
-                    Input Fields
-                </Modal.Body>
-
-                <Modal.Footer>
-                    {renderModalFooterBtns()}
-                </Modal.Footer>
+                <form onSubmit={submitForm}>
+                    <Modal.Body>
+                        {renderInputFields()}
+                    </Modal.Body>
+                    <Modal.Footer>
+                        {renderModalFooterBtns()}
+                    </Modal.Footer>
+                </form>
             </Modal>
         </>
     );
--- src/Components/Modals/LoginRegisterModal.js
+++ src/Components/Modals/LoginRegisterModal.js
@@ -1,30 +1,38 @@
 import './AuthModal.css';
 import React, {useState, useEffect} from 'react';
 import { Modal } from 'react-bootstrap';
+import InputField from '../InputFields/InputField';
 
 function LoginRegisterModal(props) {
+    //modal related states
     const [show, setShow] = useState(props.openModal);
     const [loginTabSelected, setLoginTabSelected] = useState(props.loginModalShown);
     const [registerTabSelected, setRegisterTabSelected] = useState(props.registerModalShown);
     const [confirmAccountTabSelected, setConfirmAcountTabSelected] = useState(props.confirmAccountModalShown);
 
+    //Handle Modal Events 
     function closeModal() {
+        clearForm()
         props.onChangeModalState();
     }
 
     function openLoginModal() {
+        clearForm();
         props.onSelectLoginModal();
     }
 
     function openRegisterModal() {
+        clearForm();
         props.onSelectRegisterModal();
     }
 
     function openConfirmAccountModal() {
+        clearForm();
         props.onSelectConfirmAccountModal();
     }
 
     function openForgotResetPwdModal() {
+        clearForm();
         props.onOpenForgetResetPwdModal();
     }
 
@@ -35,25 +43,76 @@ function LoginRegisterModal(props) {
         setConfirmAcountTabSelected(props.confirmAccountModalShown);
     }, [props.openModal, props.loginModalShown, props.registerModalShown, props.confirmAccountModalShown]);
 
-    //Authentication Events (Login, Register, Confirm Account)
-    function handleLogin() {
-        console.log("Login Event");
+    //Handle Input Data
+    const [data, setData] = React.useState({});
+
+    //generate react refs for input fields based on tab selected
+    function generateReactRefs() {
+        if(loginTabSelected || confirmAccountTabSelected) {
+            return [React.createRef(), React.createRef()];
+        }
+        return [React.createRef(), React.createRef(), React.createRef(), React.createRef()];
     }
 
-    function handleRegister() {
-        console.log("Register Account Event");
+    //inputref needed for each input to reference it
+    const inputRefs = React.useRef(generateReactRefs());
+
+    //update data hook with input data
+    const handleChange = (name, value) => {
+        setData(prevData => ({...prevData, [name]: value}));
     }
 
-    function handleConfirm() {
-        console.log("Confirm Account Event");
+    //submit form event, checks to see if form has errors
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        //check to see if there are errors (input validation)
+        let isValid = true;
+
+        for(let i = 0; i < inputRefs.current.length; i++) {
+            if(inputRefs.current[i].current === null) { break; }
+            const valid = inputRefs.current[i].current.validate();
+            if(!valid) {
+                isValid = false;
+            }
+        }
+
+        if(!isValid) {
+            return;
+        }
+
+        //if no errors => do actions on form submit based on what modal is active (Login, Register, Confirm)
+        if(loginTabSelected) {
+            console.log("Login Activated");
+        }
+
+        if(registerTabSelected) {
+            console.log("Register Activated");
+        }
+
+        if(confirmAccountTabSelected) {
+            console.log("Confirm Account Activated");
+        }
+
+        console.log(data);
     }
 
+    //clears data & values from forms
+    function clearForm() {
+        for(let i = 0; i < inputRefs.current.length; i++) {
+            if(inputRefs.current[i].current === null) { break; }
+            inputRefs.current[i].current.clearValue();
+        }
+        setData({});
+    }
+
+    //renders footer buttons that connect modals and do auth actions
     function renderModalFooterBtns() {
         if(loginTabSelected) {
             return (
                 <>
-                    <button className="modal-link-btn" onClick={openForgotResetPwdModal}>Forgot Password</button>
-                    <button className="modal-action-btn" onClick={handleLogin}>Login</button>
+                    <button className="modal-link-btn" type="button" onClick={openForgotResetPwdModal}>Forgot Password</button>
+                    <button className="modal-action-btn" type="submit">Login</button>
                 </>
             )
         }
@@ -61,7 +120,7 @@ function LoginRegisterModal(props) {
         if(registerTabSelected) {
             return(
                 <>
-                    <button className="modal-action-btn" onClick={handleRegister}>Register</button>
+                    <button className="modal-action-btn" type="submit">Register</button>
                 </>
             )
         }
@@ -69,7 +128,111 @@ function LoginRegisterModal(props) {
         if(confirmAccountTabSelected) {
             return(
                 <>
-                    <button className="modal-action-btn" onClick={handleConfirm}>Confirm</button>
+                    <button className="modal-action-btn" type="submit">Confirm</button>
+                </>
+            )
+        }
+
+        return null;
+    }
+
+    //renders input fields based on modal selected
+    function renderInputFields() {
+        if(loginTabSelected) {
+            return (
+                <>
+                    <InputField 
+                        ref={inputRefs.current[0]}
+                        name="username"
+                        label="Enter Username"
+                        onChange={handleChange}
+                        validation={"required|min:1|max:128"}
+                    />
+
+                    <InputField 
+                        ref={inputRefs.current[1]}
+                        name="password"
+                        label="Enter Password"
+                        type="password"
+                        onChange={handleChange}
+                        validation={"required|min:8|max:99"}
+                    />
+                </>
+            )
+        }
+
+        if(registerTabSelected) {
+            return(
+                <>
+                    <InputField 
+                        ref={inputRefs.current[0]}
+                        name="username"
+                        label="Enter Username"
+                        onChange={handleChange}
+                        validation={"required|min:1|max:128"}
+                    />
+
+                    <InputField 
+                        ref={inputRefs.current[1]}
+                        name="email"
+                        label="Enter Email"
+                        onChange={handleChange}
+                        validation={"required|email"}
+                    />
+
+                    <InputField 
+                        ref={inputRefs.current[2]}
+                        name="password"
+                        label="Enter Password"
+                        type="password"
+                        onChange={handleChange}
+                        validation={"required|min:8|max:99"}
+                    />  
+
+                    <InputField 
+                        ref={inputRefs.current[3]}
+                        name="confirm-password"
+                        label="Confirm Password"
+                        type="password"
+                        onChange={handleChange}
+                        validation={"required|min:8|max:99"}
+                    />  
+
+                    <p className="tab-informative-text">
+                        Upon registering a confirmation code will be sent to your email. 
+                        <br/>
+                        The confirmation code is valid for 24 hours. 
+                        <br/>
+                        Use the code to confirm your account registration!
+                    </p>
+                </>
+            )
+        }
+
+        if(confirmAccountTabSelected) {
+            return(
+                <>
+                    <InputField 
+                        ref={inputRefs.current[0]}
+                        name="username"
+                        label="Enter Username"
+                        onChange={handleChange}
+                        validation={"required|min:1|max:128"}
+                    />
+
+                    <InputField 
+                        ref={inputRefs.current[1]}
+                        name="confirmation code"
+                        label="Enter Confirmation Code"
+                        onChange={handleChange}
+                        validation={"required"}
+                    />
+
+                    <p className="tab-informative-text">
+                        Confirm your account here after registering.
+                        <br/>
+                        After confirming your account you will be logged in!
+                    </p>
                 </>
             )
         }
@@ -80,6 +243,7 @@ function LoginRegisterModal(props) {
     return (
         <>
             <Modal show={show} onHide={closeModal}>
+                
                 <Modal.Header closeButton className="auth-modal-header">
                     <button 
                         className={`tab-title-btn ${loginTabSelected ? 'selected':'notSelected'}`}
@@ -102,12 +266,15 @@ function LoginRegisterModal(props) {
                         <span>Confirm Account</span>
                     </button>
                 </Modal.Header>
-                <Modal.Body>
-                    Input Fields
-                </Modal.Body>
-                <Modal.Footer>
-                    {renderModalFooterBtns()}
-                </Modal.Footer>
+
+                <form id="auth-form" onSubmit={submitForm}>
+                    <Modal.Body>
+                        {renderInputFields()}
+                    </Modal.Body>
+                    <Modal.Footer>
+                        {renderModalFooterBtns()}
+                    </Modal.Footer>
+                </form>
             </Modal>
         </>
     );
