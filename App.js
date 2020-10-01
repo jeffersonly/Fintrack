f0d49e6bd122fc80ff841c9b7c78384cfb110a7f
danny-nomura <48335432+danny-nomura@users.noreply.github.com>
2020-10-01 11:22:08 -0700

    Added Split to Split branch
    Import split in app.js, add split.js to src\pages

---
+++ App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import React from 'react';
+//import NavBar from './NavBar/NavBar';
+//import LandingPage from '../Pages/LandingPage';
+import TopHeader from './NavBar/TopHeader';
+import HomeNav from './NavBar/HomeNav';
+import Summary from '../Pages/Summary';
+import Spendings from '../Pages/Spendings';
+import Savings from '../Pages/Savings';
+import Expenses from '../Pages/Expenses';
+import Transactions from '../Pages/Transactions';
+import Split from '../Pages/Split';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+function App() {
+
+    return (
+        <div className="App">
+            {/*<NavBar/>
+            <LandingPage/>*/}
+            <TopHeader />
+            <Router>
+                <HomeNav />
+                <Switch>
+                    <Route exact path="/summary">
+                        <Summary />
+                    </Route>
+                    <Route path="/spendings">
+                        <Spendings />
+                    </Route>
+                    <Route path="/transactions">
+                        <Transactions />
+                    </Route>
+                    <Route path="/expenses">
+                        <Expenses />
+                    </Route>
+                    <Route path="/savings">
+                        <Savings />
+                    </Route>
+                    <Route path="/split">
+                        <Split />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
