57c4f9c91c471010d86a35c54f462094c30a6ad2
Jefferson Ly <32202953+jeffersonly@users.noreply.github.com>
2020-10-25 18:04:38 -0700

    added drag n drop img modal, removed predictionss code for now

--- src/Components/Dropzone/Dropzone.js
+++ src/Components/Dropzone/Dropzone.js
@@ -2,34 +2,43 @@ import './Dropzone.css';
 import React, { useState, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
 import Predictions from '@aws-amplify/predictions';
+import DropzoneModal from '../Modals/DropzoneModal';
 
 function Dropzone(props) {
     const [files, setFiles] = useState([]);
     const [filesWithCost, setFilesWithCost] = useState([]);
+    const [showModal, setShowModal] = useState(false); //state of whether to show modal or not w/ images
 
     async function identifyText(acceptedFiles) {
         let arrayOfObjs = []; 
         acceptedFiles.forEach(async file => {
-            await Predictions.identify({
-                text: {
-                    source: {
-                        file
-                    }
-                }
-            })
-            .then(res => {
-                console.log(res);
-                console.log("here");
-                const totalCost = parseText(res);
-                let costAndImgObj = {
-                    totalCost: totalCost,
-                    image: file
-                };
-                arrayOfObjs.push(costAndImgObj);
-            })
-            .catch(err => console.log(err));
+            // await Predictions.identify({
+            //     text: {
+            //         source: {
+            //             file
+            //         }
+            //     }
+            // })
+            // .then(res => {
+            //     console.log(res);
+            //     console.log("here");
+            //     const totalCost = parseText(res);
+            //     let costAndImgObj = {
+            //         totalCost: totalCost,
+            //         image: file
+            //     };
+            //     arrayOfObjs.push(costAndImgObj);
+            // })
+            // .catch(err => console.log(err));
+            const totalCost = 5;
+            let costAndImgObj = {
+                totalCost: totalCost,
+                image: file
+            };
+            arrayOfObjs.push(costAndImgObj);
         });
         setFilesWithCost(arrayOfObjs);
+        setShowModal(true);
     }
 
     //function to parse text recognized by rekognition to find total cost
@@ -62,17 +71,17 @@ function Dropzone(props) {
         }
     });
 
-    const thumbs = files.map(file => (
-        <div className="thumbs-img" key={file.name}>
-            <div className="thumbs-img-inner">
-                <img
-                    src={file.preview}
-                    className="thumbnail-image"
-                    alt="receipt"
-                />
-            </div>
-        </div>
-    ));
+    // const thumbs = files.map(file => (
+    //     <div className="thumbs-img" key={file.name}>
+    //         <div className="thumbs-img-inner">
+    //             <img
+    //                 src={file.preview}
+    //                 className="thumbnail-image"
+    //                 alt="receipt"
+    //             />
+    //         </div>
+    //     </div>
+    // ));
 
     useEffect(() => () => {
         // Make sure to revoke the data uris to avoid memory leaks
@@ -81,16 +90,19 @@ function Dropzone(props) {
     }, [files, filesWithCost]);
 
     return (
-        <section className="container">
-            <div {...getRootProps({className: 'dropzone'})}>
-                <input {...getInputProps()} />
-                <p className="dropbox-text"> Drag 'n' Drop Images Here <br/> or <br/> Click to Select</p>
-                <p className="dropbox-disclaimer-text">Only *.jpeg and *.png images are accepted! <br/> Images must have a $ indicator! </p>
-            </div>
-            <aside className="thumbs-imgs-container">
-                {thumbs}
-            </aside>
-        </section>
+        <>
+            <section className="container">
+                <div {...getRootProps({className: 'dropzone'})}>
+                    <input {...getInputProps()} />
+                    <p className="dropbox-text"> Drag 'n' Drop Images Here <br/> or <br/> Click to Select</p>
+                    <p className="dropbox-disclaimer-text">Only *.jpeg and *.png images are accepted! <br/> Images must have a $ indicator! </p>
+                </div>
+                {/* <aside className="thumbs-imgs-container">
+                    {thumbs}
+                </aside> */}
+            </section>
+            <DropzoneModal show={showModal} onHide={() => setShowModal(false)} data={filesWithCost} />
+        </>
     );
 }
 
---
+++ src/Components/Modals/DropzoneModal.js
@@ -0,0 +1,49 @@
+import './DropzoneModal.css';
+import React, { useState, useEffect } from 'react';
+import { Button } from '@material-ui/core';
+import { Modal, Col, Row, Container } from 'react-bootstrap';
+import ReceiptImg from '../../Images/receipt.svg';
+import DropzoneInput from '../InputFields/DropzoneInput';
+
+export default function DropzoneModal(props) {
+    function generateItems() {
+        let arrayOfItems = [];
+        for(var i = 0; i < props.data.length; i++) {
+            let item = (
+                <Row key={i} className="itemRow">
+                    <Col xs={9} md={6}>
+                        <img 
+                            src={props.data[i] ? props.data[i].image.preview:ReceiptImg}
+                            className="itemReceiptImg"
+                            alt="receipt" 
+                        />
+                    </Col>
+                    <Col xs={9} md={6}>
+                        <DropzoneInput data={props.data[i]} />
+                    </Col>
+                </Row>
+            );
+            arrayOfItems.push(item);
+        }
+        return arrayOfItems;
+    }
+
+    return (
+        <Modal {...props} aria-labelledby="contained-modal-title-vcenter" size="lg">
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Create New Transactions
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="show-grid">
+                <Container>
+                    {generateItems()}
+                </Container>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+  
