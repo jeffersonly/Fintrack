11caf5d17ae5f9c7505fd9a1c3b8134125005e5c
danny-nomura <48335432+danny-nomura@users.noreply.github.com>
2020-11-04 11:29:26 -0800

    SplitItem Added in wrong place (corrected)

--- src/Pages/SplitItem.js
+++ src/Pages/SplitItem.js
@@ -1,38 +1,14 @@
 import React, {useState} from 'react';
-import PropTypes from 'prop-types';
 import { withRouter, useHistory} from 'react-router-dom';
 import styled from 'styled-components';
-import Grid from '@material-ui/core/Grid';
 import { 
-  Button, makeStyles, withStyles, TextField, MenuItem, Card, CardContent, Typography, Divider, InputAdornment, Input
+  Button, makeStyles, withStyles, TextField, MenuItem, Card, CardContent, Divider, InputAdornment, Input
  } from '@material-ui/core';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
-import Table from '@material-ui/core/Table';
-import Collapse from '@material-ui/core/Collapse';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import { propTypes } from 'react-bootstrap/esm/Image';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-import Box from '@material-ui/core/Box';
-import IconButton from '@material-ui/core/IconButton';
-
-
-
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
+// Split components
+import SplitItemTable from '../Components/Split/SplitItemTable';
+import SplitItemForm from '../Components/Split/SplitItemForm';
 
 const Container = styled.div`
   margin-left: 35px;
@@ -65,6 +41,14 @@ const useStyles = makeStyles({
   textfield: {
     marginBottom: "20px",
   },
+  tableTitle: {
+    fontSize: "20px",
+    fontWeight: "bold",
+    paddingBottom: "15px"
+  },
+  container: {
+    maxHeight: 400,
+  },
   create: {
     backgroundColor: "#ace1af",
     '&:hover': {
@@ -74,81 +58,30 @@ const useStyles = makeStyles({
   }
 });
 
-// Method to add new party member on click of "add party member" button
-function addPartyMember () {
-  var partyMember = document.getElementById("Party-Member").value;
-  alert(partyMember);
-}
-
-// Method to add meal to receipt on click of "add meal to receipt" button
-function addMealToReceipt () {
-  var table = document.getElementById("testTable");
-  var row = document.createElement('splitrow');
-  var td0 = document.createElement('td');
-  var td1 = document.createElement('td');
-  var td2 = document.createElement('td');
-  td0.innerHTML = document.getElementById("Party-Member").value;
-  td1.innerHTML = document.getElementById("Meal-Name").value;
-  td2.innerHTML = "$" + document.getElementById("Meal-Price").value;
-  row.appendChild(td0);
-  row.appendChild(td1);
-  row.appendChild(td2);
-  //table.children[0].appendChild(row);
-  table.children[0].insertBefore(row, table.children[0].childNodes[1]);
-  td1.value = "";
-  
-
-  // var mealName = document.getElementById("Meal-Name").value;
-  // var mealPrice = document.getElementById("Meal-Price").value;
-  // alert(mealName + " " + mealPrice);
-}
-
-// Test form
-function MyForm () {
-  return (
-    <div id="MyForm">
-      <h3>Itemize Receipt</h3>
-      <form onSubmit={addPartyMember}>
-        <label for="Name">
-          Name:
-          <input id="giveName" type="text" name="giveName"/>
-        </label>
-        <label for="Item">
-          Item:
-          <input id="giveItem" type="text" name="giveItem"/>
-        </label>
-        <label for="Price">
-          Price:
-          <input id="givePrice" type="number" name="givePrice"/>
-        </label>
-        <button type="submit" value="submit">Add Meal</button>
-      </form>
-    </div>
-  );
-}
-
-// Test table
-function MyTable () {
-  
-  return (
-    <div id="MyTable">
-      <table>
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Item</th>
-            <th>Price</th>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-}
-
 function SplitItem () {
-
+  const [rows, setRows] = useState([]);
   const history = useHistory();
   const classes = useStyles();
+  return (
+    <div>
+      <Container>
+        <SplitItemForm
+          onSubmit={data => {
+            setRows(currentRows => [
+              {
+                tName: data[0],
+                tItem: data[1],
+                tAmmount: data[2]
+              },
+              ...currentRows
+            ]);
+          }}
+        />
+        <SplitItemTable rows={rows}/>
+      </Container>
+    </div>
+  );
+  /*
     return (
       <Container>
         <Grid style={{justifyContent: 'space-evenly'}}s container spacing={2}>
@@ -209,7 +142,6 @@ function SplitItem () {
                     disableElevation
                     size="large"
                     style={{width: "100%", fontSize: "16px"}}
-                    onClick={addMealToReceipt}
                   >
                     Add Meal to Receipt
                   </Button>
@@ -225,24 +157,11 @@ function SplitItem () {
                 </ThemeProvider>
               </CardContent>
             </Card>
-            <TableContainer component={Paper}>
-              <Table aria-label="collapsible table" id="testTable" border="1">
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="center">Party Member</TableCell>
-                    <TableCell align="center">Item</TableCell>
-                    <TableCell align="center">Total ($)</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-
-                </TableBody>
-              </Table>
-            </TableContainer>
           </Grid>
         </Grid>
       </Container>
     );
+    */
   }
   
   export default withRouter(SplitItem);
